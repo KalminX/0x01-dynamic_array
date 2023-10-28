a62d2f50d7f845f7843b7d75dd5b8ab3ac3df546
KalminX <noriabure@gmail.com>
2023-10-27 22:46:18 -0700

    create a main.c file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function to implement my dynamic array
+ *
+ * Return - Returns 0 on success
+ */
+int main(void)
+{
+	return (0);
+}
